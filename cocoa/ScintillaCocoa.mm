5d888154b02ae8665464bcdfaf89e0be3a582320
Neil Hodgson <nyamatongwe@gmail.com>
2017-07-17 15:17:18 +1000

    Backport: Casts required for Unix LP64 after changes to Sci_Position/Sci_PositionU.
    Backport of changeset 6351:b7126dd1f020.

--- cocoa/ScintillaCocoa.mm
+++ cocoa/ScintillaCocoa.mm
@@ -1209,7 +1209,8 @@ void ScintillaCocoa::ClaimSelection()
  */
 NSPoint ScintillaCocoa::GetCaretPosition()
 {
-  const Sci::Line line = pdoc->LineFromPosition(sel.RangeMain().caret.Position());
+  const Sci::Line line = static_cast<Sci::Line>(
+    pdoc->LineFromPosition(sel.RangeMain().caret.Position()));
   NSPoint result;
 
   result.y = line;
@@ -1234,7 +1235,7 @@ void ScintillaCocoa::DragScroll()
   }
 
   // TODO: does not work for wrapped lines, fix it.
-  Sci::Line line = pdoc->LineFromPosition(posDrag.Position());
+  Sci::Line line = static_cast<Sci::Line>(pdoc->LineFromPosition(posDrag.Position()));
   Sci::Line currentVisibleLine = cs.DisplayFromDoc(line);
   Sci::Line lastVisibleLine = std::min(topLine + LinesOnScreen(), cs.LinesDisplayed()) - 2;
 
@@ -1342,8 +1343,8 @@ void ScintillaCocoa::StartDrag()
   PRectangle client = GetTextRectangle();
   Sci::Position selStart = sel.RangeMain().Start().Position();
   Sci::Position selEnd = sel.RangeMain().End().Position();
-  Sci::Line startLine = pdoc->LineFromPosition(selStart);
-  Sci::Line endLine = pdoc->LineFromPosition(selEnd);
+  Sci::Line startLine = static_cast<Sci::Line>(pdoc->LineFromPosition(selStart));
+  Sci::Line endLine = static_cast<Sci::Line>(pdoc->LineFromPosition(selEnd));
   Point pt;
   long startPos, endPos, ep;
   PRectangle rcSel;
