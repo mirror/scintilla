2770554d11acab2ae8969fe266f43c766303c27e
Neil <nyamatongwe@gmail.com>
2017-10-16 12:36:56 +1100

    Backport: Use const for interface CharacterIndexer and its implementation DocumentIndexer.
    Backport of changeset 6401:fccfa635ff0c.

--- src/Document.cxx
+++ src/Document.cxx
@@ -2594,7 +2594,7 @@ public:
 	~DocumentIndexer() override {
 	}
 
-	char CharAt(Sci::Position index) override {
+	char CharAt(Sci::Position index) const override {
 		if (index < 0 || index >= end)
 			return 0;
 		else
@@ -3012,7 +3012,7 @@ long BuiltinRegex::FindText(Document *doc, Sci::Position minPos, Sci::Position m
 			}
 		}
 
-		DocumentIndexer di(doc, endOfLine);
+		const DocumentIndexer di(doc, endOfLine);
 		int success = search.Execute(di, startOfLine, endOfLine);
 		if (success) {
 			pos = search.bopat[0];
@@ -3044,7 +3044,7 @@ long BuiltinRegex::FindText(Document *doc, Sci::Position minPos, Sci::Position m
 
 const char *BuiltinRegex::SubstituteByPosition(Document *doc, const char *text, Sci::Position *length) {
 	substituted.clear();
-	DocumentIndexer di(doc, static_cast<Sci::Position>(doc->Length()));
+	const DocumentIndexer di(doc, static_cast<Sci::Position>(doc->Length()));
 	search.GrabMatches(di);
 	for (int j = 0; j < *length; j++) {
 		if (text[j] == '\\') {
