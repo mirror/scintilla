923de61d67670fb0c5e3edadbae10e4b57c6dbe9
Neil <nyamatongwe@gmail.com>
2018-09-29 08:02:15 +1000

    Backport: Remove dead code.
    Backport changeset 7098:b9de068f7202.

--- src/Editor.cxx
+++ src/Editor.cxx
@@ -8204,7 +8204,6 @@ sptr_t Editor::WndProc(unsigned int iMessage, uptr_t wParam, sptr_t lParam) {
 
 	case SCI_COUNTCHARACTERS:
 		return pdoc->CountCharacters(static_cast<Sci::Position>(wParam), lParam);
-		//return pdoc->CountCharacters(static_cast<Sci::Position>(wParam), static_cast<Sci::Position>(lParam));
 
 	case SCI_COUNTCODEUNITS:
 		return pdoc->CountUTF16(static_cast<Sci::Position>(wParam), lParam);
