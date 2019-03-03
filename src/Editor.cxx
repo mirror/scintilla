e0453f3669c057174f647c6ab0fc10c8180c84c1
Neil <nyamatongwe@gmail.com>
2019-03-03 12:31:05 +1100

    Backport: Bug [#2083]. Ensure container notified if Insert pressed when caret off-screen.
    Backport of changeset 7285:9fcf43a91078.

--- src/Editor.cxx
+++ src/Editor.cxx
@@ -3792,6 +3792,7 @@ int Editor::KeyCommand(unsigned int iMessage) {
 		inOverstrike = !inOverstrike;
 		ContainerNeedsUpdate(SC_UPDATE_SELECTION);
 		ShowCaretAtCurrentPosition();
+		SetIdle(true);
 		break;
 	case SCI_CANCEL:            	// Cancel any modes - handled in subclass
 		// Also unselect text
@@ -4947,6 +4948,8 @@ void Editor::ButtonUpWithModifiers(Point pt, unsigned int curTime, int modifiers
 }
 
 bool Editor::Idle() {
+	NotifyUpdateUI();
+
 	bool needWrap = Wrapping() && wrapPending.NeedsWrap();
 
 	if (needWrap) {
@@ -7735,6 +7738,7 @@ sptr_t Editor::WndProc(unsigned int iMessage, uptr_t wParam, sptr_t lParam) {
 			inOverstrike = wParam != 0;
 			ContainerNeedsUpdate(SC_UPDATE_SELECTION);
 			ShowCaretAtCurrentPosition();
+			SetIdle(true);
 		}
 		break;
 
