229b4a6da49ad9a6f4345ef2181f55a0659b6456
Neil <nyamatongwe@gmail.com>
2019-03-28 08:25:55 +1100

    Backport: Use standard GDK_SELECTION_CLIPBOARD instead of interning "CLIPBOARD".
    Backport of changeset 7346:5e3a239e7484.

--- gtk/ScintillaGTK.cxx
+++ gtk/ScintillaGTK.cxx
@@ -130,7 +130,6 @@ enum {
 	TARGET_URI
 };
 
-GdkAtom ScintillaGTK::atomClipboard = nullptr;
 GdkAtom ScintillaGTK::atomUTF8 = nullptr;
 GdkAtom ScintillaGTK::atomString = nullptr;
 GdkAtom ScintillaGTK::atomUriList = nullptr;
@@ -1301,7 +1300,7 @@ void ScintillaGTK::RequestSelection(GdkAtom atomSelection) {
 }
 
 void ScintillaGTK::Paste() {
-	RequestSelection(atomClipboard);
+	RequestSelection(GDK_SELECTION_CLIPBOARD);
 }
 
 void ScintillaGTK::CreateCallTipWindow(PRectangle rc) {
@@ -1438,7 +1437,7 @@ GObject *ScintillaGTK::MainObject() const noexcept {
 
 void ScintillaGTK::ReceivedSelection(GtkSelectionData *selection_data) {
 	try {
-		if ((SelectionOfGSD(selection_data) == atomClipboard) ||
+		if ((SelectionOfGSD(selection_data) == GDK_SELECTION_CLIPBOARD) ||
 				(SelectionOfGSD(selection_data) == GDK_SELECTION_PRIMARY)) {
 			if ((atomSought == atomUTF8) && (LengthOfGSD(selection_data) <= 0)) {
 				atomSought = atomString;
@@ -1539,7 +1538,7 @@ void ScintillaGTK::GetSelection(GtkSelectionData *selection_data, guint info, Se
 
 void ScintillaGTK::StoreOnClipboard(SelectionText *clipText) {
 	GtkClipboard *clipBoard =
-		gtk_widget_get_clipboard(GTK_WIDGET(PWidget(wMain)), atomClipboard);
+		gtk_widget_get_clipboard(GTK_WIDGET(PWidget(wMain)), GDK_SELECTION_CLIPBOARD);
 	if (clipBoard == nullptr) // Occurs if widget isn't in a toplevel
 		return;
 
@@ -3031,7 +3030,6 @@ void ScintillaGTK::ClassInit(OBJECT_CLASS *object_class, GtkWidgetClass *widget_
 #ifdef SCI_LEXER
 	Scintilla_LinkLexers();
 #endif
-	atomClipboard = gdk_atom_intern("CLIPBOARD", FALSE);
 	atomUTF8 = gdk_atom_intern("UTF8_STRING", FALSE);
 	atomString = GDK_SELECTION_TYPE_STRING;
 	atomUriList = gdk_atom_intern("text/uri-list", FALSE);
