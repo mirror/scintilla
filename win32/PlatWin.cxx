fbae4fb7ebd024048ac899e2fb6c56f33e8d69b5
Neil <nyamatongwe@gmail.com>
2020-02-15 10:55:05 +1100

    Backport: Bug [#2138]. For pixmap, balance call to BeginDraw with EndDraw. This avoids a warning when Direct2D debugging turned on.
    Backport of changeset 7986:8c409393be53.

--- win32/PlatWin.cxx
+++ win32/PlatWin.cxx
@@ -1098,7 +1098,9 @@ void SurfaceD2D::Clear() noexcept {
 			clipsActive--;
 		}
 		if (ownRenderTarget) {
+			pRenderTarget->EndDraw();
 			pRenderTarget->Release();
+			ownRenderTarget = false;
 		}
 		pRenderTarget = nullptr;
 	}
