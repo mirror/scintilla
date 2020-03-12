3d4fafdd662a2ac7267afdaef44bad54302ebe50
Neil <nyamatongwe@gmail.com>
2020-03-13 09:03:15 +1100

    Backport: Using constexpr in UniConversion and CaseConvert.
    Backport of changeset 8004:7052c60ce1b2.

--- win32/ScintillaWin.cxx
+++ win32/ScintillaWin.cxx
@@ -1031,7 +1031,7 @@ void ScintillaWin::EscapeHanja() {
 
 	// ImmEscapeW() may overwrite uniChar[] with a null terminated string.
 	// So enlarge it enough to Maximum 4 as in UTF-8.
-	unsigned int const safeLength = UTF8MaxBytes+1;
+	constexpr size_t safeLength = UTF8MaxBytes + 1;
 	std::string oneChar(safeLength, '\0');
 	pdoc->GetCharRange(&oneChar[0], currentPos, oneCharLen);
 
