a27ff81c16f9a383ba9530aca1c4ecd16c79fee7
Neil <nyamatongwe@gmail.com>
2021-10-26 08:15:24 +1100

    Explicit constructors.

--- src/CellBuffer.cxx
+++ src/CellBuffer.cxx
@@ -37,7 +37,7 @@ struct CountWidths {
 	// from the Base Multilingual Plane and those from other planes.
 	Sci::Position countBasePlane;
 	Sci::Position countOtherPlanes;
-	CountWidths(Sci::Position countBasePlane_=0, Sci::Position countOtherPlanes_=0) noexcept :
+	explicit CountWidths(Sci::Position countBasePlane_=0, Sci::Position countOtherPlanes_=0) noexcept :
 		countBasePlane(countBasePlane_),
 		countOtherPlanes(countOtherPlanes_) {
 	}
